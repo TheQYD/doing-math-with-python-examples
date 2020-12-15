9795b80b497126d1a9e2fc0a97598f4455d47951
Qasim Dove <emailqasim@gmail.com>
2020-12-14 23:38:11 -0500

    Edited example-2.py

--- chapter_1/example-2.py
+++ chapter_1/example-2.py
@@ -1,11 +1,20 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 
 def multiplication_table(multipcand):
 
-    for multiplier in range(1, 11):
-        print('{0} x {1} = {2}'.format(multiplicand, multiplier, multiplicand*multiplier))
+  for multiplier in range(1, 11):
+    print('{0} x {1} = {2}'.format(multiplicand, multiplier, multiplicand*multiplier))
 
 if __name__ == "__main__":
 
-    multiplicand = input('Enter a number: ')
-    multiplication_table(float(multiplicand))
+  while True:
+      try: 
+        multiplicand = input('Enter a number: ')
+        multiplication_table(float(multiplicand))
+
+      except ValueError:
+        print('Please enter a number.')
+
+    answer = input('Would you like to quit? Press (y) for yes: ')
+    if answer == 'y':
+      break
