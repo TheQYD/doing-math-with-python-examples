a43e8c6a1686b7a6bf4edbcca0231dedbbf82d81
Qasim Dove <emailqasim@gmail.com>
2020-12-17 21:41:12 -0500

    updated challenge-4.py

--- chapter_1/challenge-4.py
+++ chapter_1/challenge-4.py
@@ -15,19 +15,22 @@ def divide(a,b):
   print('Result of Dividing First Fraction by the Second: {0}'.format(a/b))
 
 if __name__ == '__main__':
-  a = Fraction(input('Enter first fraction: '))
-  b = Fraction(input('Enter second fraction: '))
-  operation = input('Operation to perfrom - Add, Subtract, Multiply, Divide: ')
 
-  if operation == 'Add':
-    add(a,b)
+  while quit != 'y'
+    a = Fraction(input('Enter first fraction: '))
+    b = Fraction(input('Enter second fraction: '))
+    operation = input('Operation to perfrom - Add, Subtract, Multiply, Divide: ')
 
-  if operation == 'Subtract':
-    subtract(a,b)
+    if operation == 'Add':
+      add(a,b)
 
-  if operation == 'Multiply':
-    multiply(a,b)
+    if operation == 'Subtract':
+      subtract(a,b)
 
-  if operation == 'Divide':
-    divide(a,b)
+    if operation == 'Multiply':
+      multiply(a,b)
 
+    if operation == 'Divide':
+      divide(a,b)
+
+    quit = input('Would you like to quit? Press (y) for yes: ')
