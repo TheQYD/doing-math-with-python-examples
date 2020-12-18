060a80443e84c8bbadeb7797d23b543d6c88c87f
Qasim Dove <emailqasim@gmail.com>
2020-12-17 21:38:40 -0500

    updated challenge-3.py

--- chapter_1/challenge-3.py
+++ chapter_1/challenge-3.py
@@ -1,67 +1,69 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 
 '''
 Unit Converter
 '''
 
 def print_menu():
-    # Distance
-    print('1. Kilometers to Miles')
-    print('2. Miles to Kilometers')
+  # Distance
+  print('1. Kilometers to Miles')
+  print('2. Miles to Kilometers')
 
-    # Mass
-    print('3. Kilograms to Pounds')
-    print('4. Pounds to Kilograms')
+  # Mass
+  print('3. Kilograms to Pounds')
+  print('4. Pounds to Kilograms')
 
-    # Temperature
-    print('5. Celsius to Fahrenheit')
-    print('6. Fahrenheit to Celsius')
+  # Temperature
+  print('5. Celsius to Fahrenheit')
+  print('6. Fahrenheit to Celsius')
 
 
 # Distance
 def kilometers_to_miles():
-    kilometers = float(input('Enter distance in kilometers: '))
-    miles = kilometers / 1.609
+  kilometers = float(input('Enter distance in kilometers: '))
+  miles = kilometers / 1.609
 
-    print('Distance in miles: {0}'.format(miles))
+  print('Distance in miles: {0}'.format(miles))
 
 def miles_to_kilometers():
-    miles = float(input('Enter distance in miles: '))
-    kilometers = miles * 1.609
+  miles = float(input('Enter distance in miles: '))
+  kilometers = miles * 1.609
 
-    print ('Distance in kilometers: {0}'.format(kilometers))
+  print ('Distance in kilometers: {0}'.format(kilometers))
 
 # Mass
 def kilograms_to_pounds():
-    kilograms = float(input('Enter mass in kilograms: '))
-    pounds = kilograms * 2.20462
+  kilograms = float(input('Enter mass in kilograms: '))
+  pounds = kilograms * 2.20462
 
-    print('Weight in pounds: {0}'.format(pounds))
+  print('Weight in pounds: {0}'.format(pounds))
 
 def pounds_to_kilograms():
-    pounds = float(input('Enter weight in pounds: '))
-    kilograms = pounds / 2.20462
+  pounds = float(input('Enter weight in pounds: '))
+  kilograms = pounds / 2.20462
 
-    print('Mass in kilograms: {0}'.format(kilograms))
+  print('Mass in kilograms: {0}'.format(kilograms))
 
 # Temperature
 
 def celsius_to_fahrenheit():
-    celsius = float(input('Enter temperature in degrees Celsius: '))
-    fahrenheit = (celsius * (9.0/5.0)) + 32
+  celsius = float(input('Enter temperature in degrees Celsius: '))
+  fahrenheit = (celsius * (9.0/5.0)) + 32
 
-    print('Temperature in degrees Fahrenheit: {0}'.format(fahrenheit))
+  print('Temperature in degrees Fahrenheit: {0}'.format(fahrenheit))
 
 def fahrenheit_to_celsius():
-    fahrenheit = float(input('Enter temperature in degrees Fahrenheit: '))
+  fahrenheit = float(input('Enter temperature in degrees Fahrenheit: '))
     
-    print(fahrenheit - 32)
-    celsius = (fahrenheit - 32) * (5.0/9.0)
+  print(fahrenheit - 32)
+  celsius = (fahrenheit - 32) * (5.0/9.0)
 
-    print('Temperature in degrees Celsius: {0}'.format(celsius))
+  print('Temperature in degrees Celsius: {0}'.format(celsius))
 
 
 if __name__ == "__main__":
+  
+  while quit != 'y':
     print_menu()
     choice = str(input('Which conversion would you like to do?: '))
 
@@ -83,3 +85,4 @@ if __name__ == "__main__":
     if choice == '6':
         fahrenheit_to_celsius()
 
+    quit = input('Would you like to quit? Press (y) for yes: ')
