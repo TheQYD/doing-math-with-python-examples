b3dd0e6f1394e9dc8714ae99f714052787598055
TheQYD <emailqasim@gmail.com>
2022-03-11 00:05:01 -0500

    Safety commit.

--- chapter_2/challenge-1.py
+++ chapter_2/challenge-1.py
@@ -4,8 +4,12 @@ from matplotlib import pyplot as plt
 
 def drawGraph(x, y):
   plt.plot(x, y)
-  plt.xlabel('Time of Day')
+  plt.xlabel('Hours')
   plt.ylabel('Temperature')
   plt.title('Weather In NYC')
+  plt.show()
 
-
+if __name__ == '__main__':
+  temperature = [42, 40, 37, 39, 48, 51, 48, 46]
+  hours = range(0, len(temperature))
+  drawGraph(hours, temperature)
